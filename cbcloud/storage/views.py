7a563351d6cd93faf625558bf24184c0fb29d139
melvinchia3636 <melvinchia623600@gmail.com>
2021-08-26 18:32:16 +0800

    compatibility adjustments

--- cbcloud/storage/views.py
+++ cbcloud/storage/views.py
@@ -12,14 +12,19 @@ def requestFiles(path):
 	path = os.path.join(settings.STORAGE_DIR, path)
 	filetypes = json.load(open(staticfiles_storage.path("storage/filetype.json")))
 	if not os.path.exists(path): raise Http404
-	return sorted([{
-		"type": (type:="folder" if os.path.isdir(os.path.join(path, i)) else "file"),
+	files = []
+	for i in filter(lambda i: not i.startswith('.'), sorted(os.listdir(path))):
+		type = "folder" if os.path.isdir(os.path.join(path, i)) else "file"
+		files.append({
+		"type": type,
 		"icon": filetypes[i.split('.')[-1].lower()] if type == "file" and i.split('.')[-1].lower() in filetypes else "",
 		"name": i,
 		"last_mod": time.strftime("%d %b %Y", time.localtime(os.path.getmtime(os.path.join(path, i)))),
 		"created":  time.strftime("%d %b %Y", time.localtime(os.stat(os.path.join(path, i)).st_ctime)),
 		"size": os.path.getsize(os.path.join(path, i))
-	} for i in filter(lambda i: not i.startswith('.'), sorted(os.listdir(path)))], key=lambda i: ['folder', 'file'].index(i["type"]))
+	})
+
+	return sorted(files, key=lambda i: ['folder', 'file'].index(i["type"]))
 
 def HomeView(request):
 	hdd = psutil.disk_usage(settings.STORAGE_DIR)
