d0d9c3865ea528892f0ddcce6888060cbd750862
melvinchia3636 <melvinchia623600@gmail.com>
2021-09-14 15:39:12 +0800

    continue databases

--- cbcloud/static/database/database.js
+++ cbcloud/static/database/database.js
@@ -66,25 +66,33 @@ $("#collection-create form").submit(e => {
 
 const updateCollection = name => {
 	$('.coll').append(
-		`<div class="bg-white w-full py-3 px-4 font-bold text-xl flex items-center database-list-item transition-all cursor-pointer">
+		`<div class="bg-white w-full py-3 px-4 font-bold text-xl flex items-center database-list-item transition-all cursor-pointer new" onclick='changeCollection(this)'>
 			${name}
 		</div>`
 	);
+	$(".new").click();
+	$(".new").removeClass(".new");
 	$(".coll > div").sort(function(a,b) {
 		return ('' + $(a).text()).localeCompare($(b).text());
-   	}).appendTo(".coll")
+   	}).appendTo(".coll");
+	$('.doc-add').css("display", "flex")
 }
 
 $.ajax({
 	url: "/api/fetch-collections",
 	method: "GET",
 	success: res => {
-		$('.coll').append(res.map((e, i) => `
-		<div class="${!i ? "bg-indigo-200 text-indigo-400 flex items-center justify-between" : ""} bg-white w-full py-3 px-4 font-bold text-xl flex items-center database-list-item transition-all cursor-pointer gap-4" ${i ? `onclick='changeCollection(this)'`: ""}>
-			<span class="overflow-hidden overflow-ellipsis block">${e}</span>
-			${!i ? '<span class="iconify w-7 h-7 flex-shrink-0" data-icon="uil:angle-right"></span>' : ""}
-		</div>
-		`).join(''));
+		if (res) {
+			$('.coll').append(res.map((e, i) => `
+			<div class="${!i ? "bg-indigo-200 text-indigo-400 flex items-center justify-between" : ""} bg-white w-full py-3 px-4 font-bold text-xl flex items-center database-list-item transition-all cursor-pointer gap-4" ${i ? `onclick='changeCollection(this)'`: ""}>
+				<span class="overflow-hidden overflow-ellipsis block">${e}</span>
+				${!i ? '<span class="iconify w-7 h-7 flex-shrink-0" data-icon="uil:angle-right"></span>' : ""}
+			</div>
+			`).join(''));
+			fetchDocuments(res[0])
+		} else {
+			fetchDocuments('')
+		}
 		const ps = new PerfectScrollbar('.coll');
 	}
 })
@@ -97,7 +105,8 @@ const changeCollection = collection => {
 }
 
 const fetchDocuments = collection => {
-	$(".doc-title").text($(collection).text())
+	if (collection) $(".doc-title").text($(collection).text() || collection);
+	else $('.doc-add').css("display", "none");
 	/*
 	$.ajax({
 		url: "api-fetch-document"
@@ -105,15 +114,24 @@ const fetchDocuments = collection => {
 	*/
 }
 
+$(document).ready(function() {
+	$('select').niceSelect();
+	$('.field .option').click(e => updateFieldType(e))
+	stringField($('.field'))
+})
+
 const newDocumentPromptShow = () => {
+	$('.entry > input[type="text"]').parent().children("a").text('Auto-ID')
 	$("#document-create").removeClass("invisible").addClass('black-op-bg');
 	$(".add > div").removeClass('show');
-	const input = $('#document-create input:first-child()').val('');
+	const input = $('.entry input:first-child()').val('');
 	setTimeout(function() { input.focus() }, 500);
 }
 
 const newDocumentPromptHide = () => {
 	$("#document-create").addClass("invisible").removeClass('black-op-bg');
+	$(".fields > .field:not(:last-child)").remove();
+	appendField($('.fields > .last a'))
 }
 
 $('.entry > input[type="text"]').focus(() => {
@@ -156,3 +174,102 @@ $("#document-create input").on('input', e => {
 		isErroring = false;
 	}
 })
+
+const appendField = btn => {
+	const template = $($('#value-field').html());
+	$(btn).parent().parent().before(template);
+	$('select').niceSelect();
+	$('.field .option').click(e => updateFieldType(e));
+	stringField($(template))
+}
+
+const removeField = btn => {
+	$(btn).parents('.field').remove()
+}
+
+const updateFieldType = e => {
+	const newType = e.target.innerHTML;
+	const field = $(e.target).parents('.field');
+	switch (newType) {
+		case "string":
+			stringField(field);
+			break;
+
+		case "number":
+			numberField(field);
+			break;
+
+		case "boolean":
+			booleanField(field);
+			break;
+
+		case "null":
+			nullField(field);
+			break;
+
+		case "geopoint":
+			geoField(field);
+			break;
+	
+		default:
+			break;
+	}
+}
+
+const showFieldError = (field, content) => {
+	if (!$(field).parent().parent().children('.error').length) {
+		$(field).parent().parent().append(`
+			<div class="text-red-500 mt-1 flex items-center error">
+				<span class="iconify w-5 h-5 mr-1" data-icon="uil:exclamation-circle"></span>
+				<p class="text-red-500 font-semibold">${content}</p>
+			</div>
+		`)
+		return;
+	}
+
+	if ($(field).parent().parent().find('.error > p').text() !== content) {
+		$(field).parent().parent().find('.error > p').text(content);
+	}
+}
+
+const removeFieldError = field => {
+	$(field).parent().parent().children('.error').remove()
+}
+
+const fieldDisplay = (field, hide) => {
+	field.find('.bool, .geo').css('display', 'none');
+	field.find('* > .val:first-child').val("").css('display', hide ? "none" : "flex").parent().parent().parent().css('align-items', hide ? "center" : 'flex-start')
+}
+
+const stringField = field => {
+	fieldDisplay(field);
+	removeFieldError(field.find('.val'));
+}
+
+const nullField = field => {
+	fieldDisplay(field, true);
+	field.find('.error').remove()
+}
+
+const booleanField = field => {
+	fieldDisplay(field, true);
+	field.find('.bool').css('display', 'initial');
+	$('select').niceSelect();
+}
+
+const geoField = field => {
+	fieldDisplay(field, true);
+	field.find('.geo').css('display', 'flex');
+}
+
+const numberField = field => {
+	fieldDisplay(field)
+	const checkNum = (f, emptyCheck) => {
+		const value = $(f).val().trim();
+		if (!value && emptyCheck) showFieldError(f, "Required")
+		else if (value &&!(!isNaN(parseFloat(value)) && isFinite(value))) showFieldError(f, "Must be a number")
+		else removeFieldError(f);
+	}
+	checkNum($(field).find('.val'), false);
+	$(field).find('.val').off('input').on('input', e => checkNum(e.target, true))
+}
